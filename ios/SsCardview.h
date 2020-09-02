2ec85edbc65843a65e9362a09b19fae1ff021434
SHEN <>
2020-09-02 09:49:11 +0800

    dev

--- ios/SsCardview.h
+++ ios/SsCardview.h
@@ -1,3 +1,11 @@
+//
+//  SsCardview.h
+//  SsCardview
+//
+//  Created by SHEN on 2020/9/2.
+//  Copyright © 2020 SHEN. All rights reserved.
+//
+
 #import <React/RCTViewManager.h>
 
 @interface SsCardview : RCTViewManager
--- ios/SsCardview.m
+++ ios/SsCardview.m
@@ -1,3 +1,11 @@
+//
+//  SsCardview.m
+//  SsCardview
+//
+//  Created by SHEN on 2020/9/2.
+//  Copyright © 2020 SHEN. All rights reserved.
+//
+
 #import "SsCardview.h"
 
 @implementation SsCardview
@@ -9,7 +17,7 @@ RCT_EXPORT_MODULE()
     // TODO: Implement some actually useful functionality
     UILabel * label = [[UILabel alloc] init];
     [label setTextColor:[UIColor redColor]];
-    [label setText: @"hello *****"];
+    [label setText: @"hello 12345"];
     [label sizeToFit];
     UIView * wrapper = [[UIView alloc] init];
     [wrapper addSubview:label];
